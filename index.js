adfac749ecdc8ff5f5936db21b381b122a8c0e91
stvnhu <stvnhng1@gmail.com>
2026-09-11 18:33:26 +0100

    moved start and end logging out of automate function changed logging to indicate only 1 discord tab should be open added a loop after completion to start again (for more than 400 pages of deletion) increased timings to prevent failed deletions try catch block to prevent crashes: - failing to find the last page of search results - failing to delete message will skip to next message - failing to find previous button will end loop

--- index.js
+++ index.js
@@ -8,84 +8,82 @@ function wait(ms) {
 }
 
 async function automate(browser) {
-
-    console.log('task begin');
-
     const pages = await browser.pages();
     const page = await pages.find(page => page.url().includes('discord'));
 
     if (!page) {
-        console.log('discord tab not open');
+        console.log('0 or more than 1 discord tabs are open');
         return;
     }
 
     if (!(await page.$('#search-results-0'))) {
-        console.log('search results unavailable');
+        console.log('search results are not open');
         return;
-    }
-
-    const page_buttons = await page.$$('div[aria-label^="Page "]');
-    const last_page = await page_buttons.at(-1);
-
-    if (last_page) {
-        await last_page.click();
-        await wait(3000);
     }
 
     let page_number = 1;
     let msgs_deleted = 0
 
     for (; ;) {
+        try {
+            const page_buttons = await page.$$('div[aria-label^="Page "]');
+            const last_page = await page_buttons.at(-1);
+            await last_page.click();
+            await wait(3000);
+        }
+        catch { }
 
-        const elements = await page.$$('li[id^="search-results-"]');
+        for (; ;) {
 
-        for (let msg_number = 0; msg_number < elements.length; ++msg_number) {
+            const elements = await page.$$('li[id^="search-results-"]');
 
-            const click_target = await elements[elements.length - msg_number - 1].$('h2[class^="header_"]');
-            if (!click_target) {
-                continue;
+            for (let msg_number = 0; msg_number < elements.length; ++msg_number) {
+                try {
+                    const click_target = await elements[elements.length - msg_number - 1].$('h2[class^="header_"]');
+                    await click_target.click()
+                    await wait(1200);
+
+                    await click_target.click({ button: 'right' });
+                    await wait(200);
+
+                    const message_delete_id = '#message-delete';
+                    await page.waitForSelector(message_delete_id, { timeout: 2000, visible: true });
+                    await page.keyboard.down('Shift');
+                    await page.click(message_delete_id);
+                    await page.keyboard.up('Shift');
+                    ++msgs_deleted;
+                    console.log(`(total | page | msg): ${msgs_deleted} | ${page_number} | ${msg_number + 1}`);
+                    await wait(600);
+                }
+                catch {
+                    continue;
+                }
             }
-            await click_target.click()
-            await wait(600);
-
-            await click_target.click({ button: 'right' });
-            await wait(200);
-
-            const message_delete_id = '#message-delete';
             try {
-                await page.waitForSelector(message_delete_id, { timeout: 2000, visible: true });
-                await page.keyboard.down('Shift');
-                await page.click(message_delete_id);
-                await page.keyboard.up('Shift');
-                ++msgs_deleted;
-                console.log(`(total | page | msg): ${msgs_deleted} | ${page_number} | ${msg_number + 1}`);
+                const prev_button = await page.$('button[rel="prev"]');
+                const disabled_property = await prev_button.getProperty('disabled');
+                const is_disabled = await disabled_property.jsonValue();
+                if (is_disabled) {
+                    break;
+                }
+                prev_button.click();
+                ++page_number
+                await wait(3000);
             }
             catch {
-                continue;
+                return;
             }
-            await wait(200);
         }
-        const prev_button = await page.$('button[rel="prev"]');
-        if (!prev_button) {
-            break;
-        }
-        const is_disabled = await (await prev_button.getProperty('disabled')).jsonValue();
-        if (is_disabled) {
-            break;
-        }
-        prev_button.click();
-        ++page_number
-        await wait(3000);
     }
-    console.log('task complete');
 }
 
 (async () => {
+    console.log('task begin');
     const browser = await puppeteer.connect({
         browserURL: 'http://127.0.0.1:9222',
         defaultViewport: null,
     });
     await automate(browser);
     browser.disconnect();
+    console.log('task complete');
 })();
-
